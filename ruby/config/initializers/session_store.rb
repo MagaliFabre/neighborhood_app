b16852e87d13a672839aa7b1d4093e3fe96df8c1
Magali Fabre <magali-fabre@hotmail.fr>
2024-06-15 15:04:49 +0200

    update env

--- ruby/config/initializers/session_store.rb
+++ ruby/config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 if Rails.env == "production"
   # Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "neighborhood-authentication-app-api.herokuapp.com"
   # Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "localhost"
-  Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "https://neighborhood-app-front.onrender.com/"
+  Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "neighborhood-app-front.onrender.com"
 
 else
   Rails.application.config.session_store :cookie_store, key: "_authentication_app"
