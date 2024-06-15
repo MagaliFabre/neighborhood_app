86a8f8985b2e6f4c341a43cab8067ee1e0a09f64
Magali Fabre <magali-fabre@hotmail.fr>
2024-06-15 15:20:23 +0200

    update env

--- ruby/config/initializers/cors.rb
+++ ruby/config/initializers/cors.rb
@@ -13,6 +13,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "https://neighborhood-app-front.onrender.com"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    # headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 end
