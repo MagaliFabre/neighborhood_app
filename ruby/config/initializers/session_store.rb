bc4764d569e2aafa2c27b65ceb745dfd521b8b3c
Magali Fabre <magali-fabre@hotmail.fr>
2024-06-15 18:13:36 +0200

    domain fix

--- ruby/config/initializers/session_store.rb
+++ ruby/config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 if Rails.env == "production"
   # Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "neighborhood-authentication-app-api.herokuapp.com"
   # Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "localhost"
-  Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: ".onrender.com"
+  Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "server.gr1moire.net"
 else
   Rails.application.config.session_store :cookie_store, key: "_authentication_app"
 end
